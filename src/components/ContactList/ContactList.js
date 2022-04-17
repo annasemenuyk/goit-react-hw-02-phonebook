b511f29d873f395b388e123a60cbbfa04e103b06
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2022-04-17 22:22:15 +0300

    styles

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ContactList.module.css';
-import { nanoid } from 'nanoid';
 
 const ContactList = ({ onFilteredContacts, onDeleteContact }) => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import ReactDOM from 'react-dom';
 import './index.css';
+import App from './components/App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
